b7b9a6d8ab90a73c9fe8ebe6c2cc2f4dfc067d2c
minghui.y <someonemh@yeah.net>
2021-12-29 23:50:34 +0800

    xml数据源驱动完成

---
+++ aligo-flow-core/src/main/java/com/aligo/flow/config/AligoFlowAutoConfiguration.java
@@ -0,0 +1,7 @@
+package com.aligo.flow.config;/**
+*  配置类
+*  @author minghui.y
+*  @create 2021-12-29 11:27 下午
+**/
+public class AligoFlowAutoConfiguration {
+}
---
+++ aligo-flow-core/src/main/java/com/aligo/flow/config/FlowDriverConfig.java
@@ -0,0 +1,7 @@
+package com.aligo.flow.config;/**
+*  
+*  @author minghui.y
+*  @create 2021-12-29 11:30 下午
+**/
+public class FlowDriverConfig {
+}
